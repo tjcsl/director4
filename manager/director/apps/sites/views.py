162ee99fb5e1baa17af72f75aad6feaa58bb0b9e
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-11-19 17:09:37 -0500

    director: add type annotations to view

--- manager/director/apps/sites/views.py
+++ manager/director/apps/sites/views.py
@@ -2,9 +2,10 @@
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 @login_required
-def index_view(request):
+def index_view(request: HttpRequest) -> HttpResponse:
     return render(request, "sites/list.html")
