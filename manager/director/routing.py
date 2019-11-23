6eb6a52a12731da162b0adf98000d61b69448eb4
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-11-23 13:13:28 -0500

    Close all incoming websocket connections that don't match another route

--- manager/director/routing.py
+++ manager/director/routing.py
@@ -1,17 +1,33 @@
+from typing import Optional
+
 from channels.auth import AuthMiddlewareStack
+from channels.generic.websocket import WebsocketConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.urls import path
 
 from director.apps.sites.consumers import SiteConsumer
 
+
+class WebsocketCloseConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.close()
+
+    def receive(self, text_data: Optional[str] = None, bytes_data: Optional[bytes] = None):
+        pass
+
+    def disconnect(self, code):
+        pass
+
+
 application = ProtocolTypeRouter(
     {
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 [
-                    # URLRouter just takes standard Django path() or url() entries.
                     path("sites/<int:site_id>/", SiteConsumer),
+                    path("<path:path>", WebsocketCloseConsumer),
                 ]
             )
         )
