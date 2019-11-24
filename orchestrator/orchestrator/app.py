8eb2eeeb3e6f256db72c95caa23955bc913dc142
Theo Ouzhinski <touzhinski@gmail.com>
2019-11-23 19:51:58 -0500

    orchestrator: comment unused imports

--- orchestrator/orchestrator/app.py
+++ orchestrator/orchestrator/app.py
@@ -1,16 +1,18 @@
 # SPDX-License-Identifier: MIT
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
-import logging
+# import logging
 
-from flask import Flask, jsonify, redirect, request, url_for
+from flask import Flask  # , jsonify, redirect, request, url_for
 
 # from flask_cors import CORS
 
+"""
 try:
     from secret import *
 except ValueError:
     pass
+"""
 
 app = Flask(__name__)
 
