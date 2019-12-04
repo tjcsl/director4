14f43601f67bfd8676b4fe44707d4fd1a0e3f291
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-04 10:13:06 -0500

    manager: Mostly finish site list view; begin site information view
    All the links are dead, but it's a start

--- manager/director/apps/sites/urls.py
+++ manager/director/apps/sites/urls.py
@@ -9,5 +9,20 @@ app_name = "sites"
 
 urlpatterns = [
     path("", views.index_view, name="index"),
+    path("create/", views.dummy_view, name="create"),
+    path("create/webdocs/", views.dummy_view, name="create_webdocs"),
     path("<int:site_id>/", views.info_view, name="info"),
+    path("<int:site_id>/edit/", views.dummy_view, name="edit"),
+    path("<int:site_id>/delete/", views.dummy_view, name="delete"),
+    path("<int:site_id>/terminal/", views.dummy_view, name="web_terminal"),
+    path("<int:site_id>/files/", views.dummy_view, name="editor"),
+    path("<int:site_id>/install/", views.dummy_view, name="install_options"),
+    path("<int:site_id>/config/", views.dummy_view, name="config"),
+    path("<int:site_id>/permission/", views.dummy_view, name="permissions"),
+    path("<int:site_id>/database/create/", views.dummy_view, name="create_database"),
+    path("<int:site_id>/database/edit/", views.dummy_view, name="edit_database"),
+    path("<int:site_id>/database/delete/", views.dummy_view, name="delete_database"),
+    path("<int:site_id>/process/create/", views.dummy_view, name="create_process"),
+    path("<int:site_id>/process/edit/", views.dummy_view, name="edit_process"),
+    path("<int:site_id>/process/delete/", views.dummy_view, name="delete_process"),
 ]
--- manager/director/apps/sites/views.py
+++ manager/director/apps/sites/views.py
@@ -1,22 +1,40 @@
 # SPDX-License-Identifier: MIT
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
+from typing import Optional
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Site
 
 
 @login_required
 def index_view(request: HttpRequest) -> HttpResponse:
     show_all = request.user.is_superuser and bool(request.GET.get("all"))
+
+    sites = list(Site.objects.filter(users=request.user).order_by("name"))
+    if show_all:
+        sites.extend(Site.objects.exclude(users=request.user).order_by("name"))
+
     context = {
         "show_all": show_all,
+        "sites": sites,
     }
     return render(request, "sites/list.html", context)
 
 
+# Used for routes that do not have views written but need to be linked to
 @login_required
-def info_view(  # pylint: disable=unused-argument
-    request: HttpRequest, site_id: int
+def dummy_view(  # pylint: disable=unused-argument
+    request: HttpRequest, site_id: Optional[int] = None
 ) -> HttpResponse:
-    return render(request, "sites/info.html")
+    return HttpResponse("")
+
+
+@login_required
+def info_view(request: HttpRequest, site_id: int) -> HttpResponse:
+    site = get_object_or_404(Site, id=site_id)
+    context = {"site": site}
+    return render(request, "sites/info.html", context)
