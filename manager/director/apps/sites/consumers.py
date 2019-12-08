1fa0938a95b709a439b7158029c6cfdda0f07160
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-08 08:28:36 -0500

    manager: Begin work on SiteConsumer

--- manager/director/apps/sites/consumers.py
+++ manager/director/apps/sites/consumers.py
@@ -1,21 +1,30 @@
 # SPDX-License-Identifier: MIT
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
-from typing import Any
+from typing import Any, Optional
 
 from channels.generic.websocket import JsonWebsocketConsumer
+
+from .models import Site
 
 
 class SiteConsumer(JsonWebsocketConsumer):
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
-        self.site = None
+        self.site: Optional[Site] = None
         self.connected = False
 
     def connect(self) -> None:
-        site_id = self.scope["url_route"]["kwargs"][  # pylint: disable=unused-variable # noqa
-            "site_id"
-        ]
+        if not self.scope["user"].is_authenticated:
+            self.close()
+            return
+
+        site_id = int(self.scope["url_route"]["kwargs"]["site_id"])
+        try:
+            self.site = Site.objects.get(users=self.scope["user"], id=site_id)
+        except Site.DoesNotExist:
+            self.close()
+            return
 
         self.connected = True
         self.accept()
@@ -32,6 +41,15 @@ class SiteConsumer(JsonWebsocketConsumer):
 
     def send_site_info(self) -> None:
         if self.connected:
+            assert self.site is not None
+
             self.send_json(
-                {"site_info": {}, }
+                {
+                    "site_info": {
+                        "name": self.site.name,
+                        "description": self.site.description,
+                        "port": self.site.port,
+                        "type": self.site.type,
+                    },
+                }
             )
