365fbaaa952afeb528b9b7ae3df9e83f7879f0ab
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-07 23:10:56 -0500

    manager: Add basic demo view

--- manager/director/apps/sites/views.py
+++ manager/director/apps/sites/views.py
@@ -1,12 +1,16 @@
 # SPDX-License-Identifier: MIT
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
+import json
 from typing import Optional
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
+from ...utils.appserver import AppserverRequestError, appserver_open_http_request
 from .models import Site
 
 
@@ -31,6 +35,29 @@ def dummy_view(  # pylint: disable=unused-argument
     request: HttpRequest, site_id: Optional[int] = None
 ) -> HttpResponse:
     return HttpResponse("")
+
+
+@require_POST
+@login_required
+def demo_view(request: HttpRequest) -> HttpResponse:
+    try:
+        # Connect to a random appserver
+        resp = appserver_open_http_request(-1, "/demo", method="POST")
+        data = resp.json()
+    except AppserverRequestError as ex:
+        messages.error(request, "Error connecting to appserver: {}".format(ex))
+    except json.JSONDecodeError as ex:
+        messages.error(request, "Invalid response from appserver: {}".format(ex))
+    else:
+        if not isinstance(data, dict) or "success" not in data:
+            messages.error(request, "Invalid response from server")
+        else:
+            if data["success"]:
+                messages.success(request, "Successfully created Docker container on appserver {}.".format(resp.appserver_index))
+            else:
+                messages.error(request, "Error creating Docker container.")
+
+    return redirect("auth:index")
 
 
 @login_required
