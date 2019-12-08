7b6f1ed386dfd06fe00895d68faaa8b5e0ca707e
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-08 08:01:19 -0500

    manager: Blackify fabfile.py and manage.py

--- manager/fabfile.py
+++ manager/fabfile.py
@@ -2,25 +2,34 @@ import os
 
 from fabric import task
 
-env = {key: os.environ[key] for key in ["LANG", "PATH", "TERM", "USER", "LOGNAME", "HOME", "LS_COLORS"] if key in os.environ}
+env = {
+    key: os.environ[key]
+    for key in ["LANG", "PATH", "TERM", "USER", "LOGNAME", "HOME", "LS_COLORS"]
+    if key in os.environ
+}
 
 my_hosts = ["localhost"]
+
 
 @task
 def install(c):
     c.run("pipenv install --dev --deploy", env=env, pty=True)
 
+
 @task
 def runserver(c, port=8080):
     c.run("pipenv run python ./manage.py runserver 0.0.0.0:{}".format(port), env=env, pty=True)
+
 
 @task
 def shell(c):
     c.run("pipenv run python ./manage.py shell_plus", env=env, pty=True)
 
+
 @task
 def makemigrations(c):
     c.run("pipenv run python ./manage.py makemigrations", env=env, pty=True)
+
 
 @task
 def migrate(c):
--- manager/manage.py
+++ manager/manage.py
@@ -8,7 +8,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'director.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "director.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,5 +20,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
