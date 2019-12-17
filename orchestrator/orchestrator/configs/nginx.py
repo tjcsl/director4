3f66e3b839bfff8da7285ec87df6a818720fa684
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-17 15:51:52 -0500

    orchestrator: Fix Nginx config generation

--- orchestrator/orchestrator/configs/nginx.py
+++ orchestrator/orchestrator/configs/nginx.py
@@ -3,6 +3,7 @@
 
 import os
 import re
+import subprocess
 from typing import Any, Dict, Optional
 
 import jinja2
@@ -29,7 +30,7 @@ def update_nginx_config(site_id: int, data: Dict[str, Any]) -> Optional[str]:
         return "Invalid name"
     if new_data["primary_url_base"] is not None and (
         not isinstance(new_data["primary_url_base"], str)
-        or re.search(r"^https?://[-a-zA-Z0-9.]+(/[-_a-zA-Z0-9.~])+$", new_data["primary_url_base"])
+        or re.search(r"https?://[-a-zA-Z0-9.]+(:\d+)?(/([-_a-zA-Z0-9.~]+/)*[-_a-zA-Z0-9.~]*)?$", new_data["primary_url_base"])
         is None
     ):
         return "Invalid primary URL"
@@ -38,8 +39,13 @@ def update_nginx_config(site_id: int, data: Dict[str, Any]) -> Optional[str]:
     for domain in new_data["no_redirect_domains"]:
         if (
             not isinstance(domain, str)
-            or re.search(r"^[a-z][a-z0-9]*(-[a-z0-9]+)*(\.[a-z][a-z0-9]*(-[a-z0-9]+)*)+$", domain)
-            is None
+            or (
+                re.search(r"^[a-z][a-z0-9]*(-[a-z0-9]+)*(\.[a-z][a-z0-9]*(-[a-z0-9]+)*)+$", domain)
+                is None
+                and
+                re.search(r"^((\d+\.){3}\d+|([0-9a-fA-F]|:):[0-9a-fA-F:]*)$", domain)
+                is None
+            )
         ):
             return "Invalid 'no redirect' domain {!r}".format(domain)
     if not isinstance(new_data["port"], int) or new_data["port"] < 10000:
@@ -52,10 +58,27 @@ def update_nginx_config(site_id: int, data: Dict[str, Any]) -> Optional[str]:
 
     text = nginx_template.render(variables)
 
+    nginx_config_path = "/etc/nginx/director.d/site-{}.conf".format(site_id)
+
     try:
-        with open("/etc/nginx/director.d/site-{}.conf".format(site_id), "w") as f_obj:
+        with open(nginx_config_path, "w") as f_obj:
             f_obj.write(text)
     except OSError as ex:
         return "Error writing Nginx config: {}".format(ex)
-    else:
-        return None
+
+    try:
+        res = subprocess.run(settings.NGINX_CONFIG_CHECK_COMMAND, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, check=True)
+    except (OSError, subprocess.CalledProcessError):
+        try:
+            os.remove(nginx_config_path)
+        except OSError:
+            return "Error checking Nginx config for errors (also unable to remove site config)"
+        else:
+            return "Error checking Nginx config for errors (site config has been removed)"
+
+    try:
+        res = subprocess.run(settings.NGINX_CONFIG_RELOAD_COMMAND, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, check=True)
+    except (OSError, subprocess.CalledProcessError):
+        return "Error reloading Nginx config"
+
+    return None
