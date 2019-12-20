c0f9b8b5bbd31d58532bb3c2255f7417ac5ce1fb
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-20 10:03:41 -0500

    manager: Add models to admin interface

--- manager/director/apps/sites/admin.py
+++ manager/director/apps/sites/admin.py
@@ -1,4 +1,18 @@
 # SPDX-License-Identifier: MIT
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
-from django.contrib import admin  # pylint: disable=unused-import # noqa
+from django.contrib import admin
+
+from .models import DockerImage, DockerImageExtraPackage, Domain, DatabaseHost, Database, Site, Operation, Action
+
+admin.site.register(DockerImage)
+admin.site.register(DockerImageExtraPackage)
+admin.site.register(Domain)
+
+admin.site.register(DatabaseHost)
+admin.site.register(Database)
+
+admin.site.register(Site)
+
+admin.site.register(Operation)
+admin.site.register(Action)
