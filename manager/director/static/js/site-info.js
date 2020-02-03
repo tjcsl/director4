02d55604749f25ba0ae35421044f8a1c0730a489
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-02-02 23:34:19 -0500

    manager: Fix "databases" section on site info page

---
+++ manager/director/static/js/site-info.js
@@ -0,0 +1,26 @@
+jQuery.fn.selectText = function() {
+    var doc = document;
+    var element = this[0];
+    var range;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
+
+$(document).ready(function() {
+    $("#database-url").click(function() {
+        $("#database-pass").removeClass("hide");
+    }).dblclick(function() {
+        $(this).selectText();
+    }).on("blur", function() {
+        $("#database-pass").addClass("hide");
+    });
+});
