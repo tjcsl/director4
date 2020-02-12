895eb672318770b53eafea7b91fe174c8ab08c6b
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-02-11 20:43:06 -0500

    orchestrator: Add missing SPDX license identifiers

--- orchestrator/orchestrator/exceptions.py
+++ orchestrator/orchestrator/exceptions.py
@@ -1,2 +1,5 @@
+# SPDX-License-Identifier: MIT
+# (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
+
 class OrchestratorActionError(Exception):
     pass
--- orchestrator/orchestrator/settings/secret_dev.py
+++ orchestrator/orchestrator/settings/secret_dev.py
@@ -1,1 +1,4 @@
+# SPDX-License-Identifier: MIT
+# (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
+
 DEBUG = True
