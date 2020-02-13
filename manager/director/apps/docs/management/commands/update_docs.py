1e9ab351ac47936246d47d32025f83c6cfaf8f53
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-02-13 15:51:34 -0500

    manager: Add command to update docs

---
+++ manager/director/apps/docs/management/commands/update_docs.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: MIT
+# (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
+
+import subprocess
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Pulls the latest version of the Director docs"
+
+    def handle(self, *args, **options) -> None:
+        subprocess.run(
+            ["git", "pull"], cwd=settings.DIRECTOR_DOCS_DIR, check=True,
+        )
