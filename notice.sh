dbb92cf633b2112ca9b476246dd574d47d96c71c
Theo Ouzhinski <touzhinski@gmail.com>
2020-03-02 15:42:47 -0500

    meta: add start of 3rd party notices
    for licensing purposes

---
+++ notice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+title="$1"
+echo "%% $title NOTICES AND INFORMATION BEGIN HERE
+=============================================================
+=============================================================
+%% $title NOTICES AND INFORMATION END HERE
+"
