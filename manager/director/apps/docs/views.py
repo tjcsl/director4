df5c0c2c377d961227cb6abfd756a149672fb124
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-03-19 15:16:44 -0400

    manager: Only allow GET requests to docs page

--- manager/director/apps/docs/views.py
+++ manager/director/apps/docs/views.py
@@ -4,10 +4,12 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
 from .utils import load_doc_page
 
 
+@require_GET
 @login_required
 def doc_page_view(request: HttpRequest, page_name: str = "") -> HttpResponse:
     metadata, text_html = load_doc_page(page_name)
