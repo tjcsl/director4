6ba1278ae61a7963edd88de0cd856310ade7b730
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-04-05 14:53:51 -0400

    manager(docs): Clean up slashes in page URLs

--- manager/director/apps/docs/views.py
+++ manager/director/apps/docs/views.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: MIT
 # (c) 2019 The TJHSST Director 4.0 Development Team & Contributors
 
+import re
 from typing import Any, Dict, List, Tuple
 
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.decorators.http import require_GET
 
 from .utils import get_page_title, iter_page_names, load_doc_page
@@ -14,6 +15,9 @@ from .utils import get_page_title, iter_page_names, load_doc_page
 @require_GET
 @login_required
 def doc_page_view(request: HttpRequest, page_name: str = "") -> HttpResponse:
+    if "//" in page_name or page_name.startswith("/") or page_name.endswith("/"):
+        return redirect("docs:doc_page", re.sub(r"/+", "/", page_name.strip("/")))
+
     metadata, text_html = load_doc_page(page_name)
 
     if text_html is None:
