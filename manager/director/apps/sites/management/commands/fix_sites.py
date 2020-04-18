50d1d8d2649caa1f7a23c19ffad0a9fef150b3a2
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-04-18 14:03:18 -0400

    manager: Add fix_sites management command

---
+++ manager/director/apps/sites/management/commands/fix_sites.py
@@ -0,0 +1,40 @@
+import time
+from typing import Any
+
+from django.core.management.base import BaseCommand, CommandError
+
+from director.apps.sites.models import Operation, Site
+from director.apps.sites.operations import fix_site
+
+
+class Command(BaseCommand):
+    help = "Run 'fix' operations on all sites"
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        for site in Site.objects.all().order_by("id"):
+            if Operation.objects.filter(site=site).exists():
+                self.stdout.write(
+                    "Site {} (id {}) already has an operation running; waiting for "
+                    "completion...".format(site.name, site.id)
+                )
+
+                self.wait_for_operation(site)
+
+            print("Running 'fix' operation on site {} (id {})".format(site.name, site.id))
+            fix_site(site)
+
+            self.wait_for_operation(site)
+
+    def wait_for_operation(self, site: Site) -> None:
+        while True:
+            try:
+                operation = Operation.objects.get(site_id=site.id)
+            except Operation.DoesNotExist:
+                # Finished
+                print("Operation finished on site {} (id {})".format(site.name, site.id))
+                return
+
+            if operation.has_failed:
+                raise CommandError("Operation failed on site {} (id {})".format(site.name, site.id))
+
+            time.sleep(1)
