e663adeeb53f07bb1f5dff45b8e0fe332055eb74
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-04-23 22:26:48 -0400

    scripts: Update for router/shell servers

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname -- "$(readlink -f "$0")")/.."
 
-for dname in manager orchestrator; do
+for dname in manager orchestrator router shell; do
     echo "Installing packages for $dname"
     (cd "$dname" && pipenv install --dev --deploy)
 done
--- scripts/start-servers.sh
+++ scripts/start-servers.sh
@@ -3,6 +3,7 @@ cd "$(dirname -- "$(readlink -f "$0")")/.."
 
 tmux new-session -s servers -c manager \
     "expect ../scripts/launch-and-interact.exp 'fab runserver'" ';' \
-    split-window -p 75 -c manager "expect ../scripts/launch-and-interact.exp 'fab celery'" ';' \
-    split-window -p 67 -c orchestrator "expect ../scripts/launch-and-interact.exp 'fab runserver'" ';' \
-    split-window -p 50 -c orchestrator "expect ../scripts/launch-and-interact.exp 'fab wsserver'"
+    split-window -p 80 -c manager "expect ../scripts/launch-and-interact.exp 'fab celery'" ';' \
+    split-window -p 75 -c orchestrator "expect ../scripts/launch-and-interact.exp 'fab runserver'" ';' \
+    split-window -p 67 -c orchestrator "expect ../scripts/launch-and-interact.exp 'fab wsserver'" ';' \
+    split-window -p 50 -c shell "expect ../scripts/launch-and-interact.exp 'fab server'"
