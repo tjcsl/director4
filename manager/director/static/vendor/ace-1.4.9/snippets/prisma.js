2f98bb842344fc97bd710f690997b46dc2b9be03
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-04-24 10:44:14 -0400

    manger: Update vendored dependencies

--- manager/director/static/vendor/ace-1.4.9/snippets/prisma.js
+++
@@ -1,8 +0,0 @@
-define("ace/snippets/prisma",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="prisma"});                (function() {
-                    window.require(["ace/snippets/prisma"], function(m) {
-                        if (typeof module == "object" && typeof exports == "object" && module) {
-                            module.exports = m;
-                        }
-                    });
-                })();
-            
--- manager/director/static/vendor/ace-1.4.9/snippets/qml.js
+++
@@ -1,8 +0,0 @@
-define("ace/snippets/qml",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="qml"});                (function() {
-                    window.require(["ace/snippets/qml"], function(m) {
-                        if (typeof module == "object" && typeof exports == "object" && module) {
-                            module.exports = m;
-                        }
-                    });
-                })();
-            
