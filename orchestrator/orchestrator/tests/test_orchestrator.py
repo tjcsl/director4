689ce06f92af3c1ef20672b246a40a1b821b3eef
Ethan Nguyen <etnguyen03@hotmail.com>
2020-11-21 10:13:48 -0500

    ci/test: add basic test suites for everything

---
+++ orchestrator/orchestrator/tests/test_orchestrator.py
@@ -0,0 +1,13 @@
+import unittest
+
+from ..app import app
+
+
+class OrchestratorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        app.testing = True
+        self.client = app.test_client()
+
+    def test_ping(self) -> None:
+        request = self.client.get("/ping")
+        self.assertIn(b"Pong", request.data)
